9bed30468035b8adc11b4ed31d84fe625a384b59
Daniel Whittingham <zy1pk0z7@students.codeinstitute.net>
2024-11-03 20:34:37 +0000

    JS restart

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,5 @@
 document.addEventListener('DOMContentLoaded',function(){
     let buttons = document.getElementsByTagName('button');
 
+})
 
-/**
-* Gets the current score from the DOM and increments it by 1
-*/
-function incrementScore() {
-
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-  }
-  
-  /**
-  * Gets the current tally of incorrect answers from the DOM and increments it by 1
-  */
-  function incrementWrongAnswer() {
-  
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-    
-  }
