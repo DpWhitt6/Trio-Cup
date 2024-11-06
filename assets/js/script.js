f96c1ffdffd11edde89d24f13cfac8d473afad5b
Daniel Whittingham <zy1pk0z7@students.codeinstitute.net>
2024-11-06 22:54:33 +0000

    attempted code to move class elements between sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,13 @@ document.addEventListener('DOMContentLoaded',function() {
         button.addEventListener('click', function(){
           if (this.getAttribute('type')=== 'submitWelcome') {
             handleSubmit();
-          } else if (this.getAttribute('data-type') === 'submit') {
+            /**
+             * Insert code to remove hidden css code in the HTML in the game section and add it to welcome page
+             * alert saying welcome "username"
+             * Game length options best of 5,10 or unlimited - If 5 games player then hide all code except game over
+             * add reset option to gameover - two choices reset game (back to 5/10/unlimited) or welcome page  
+             */
+          } else if (this.getAttribute('data-type') === 'submitCup') {
                 alert ('You Clicked Submit!');
                 runGame()
             } else if (this.getAttribute('data-type')==='cupChoice'){
@@ -21,28 +27,29 @@ document.addEventListener('DOMContentLoaded',function() {
   }
 })
 
-// Welcome Page 
+let form = document.getElementById('login-form');
+form.addEventListener('submit', handleSubmit); //should this element let onto openGame.. how
+
+//Form Filled 
 function handleSubmit(event) {
   // Prevent the default submit action (more on this in a couple units)
   event.preventDefault();
       let firstName = document.getElementById('firstName');
       let lastName = document.getElementById('lastName');
       let username = document.getElementById('username');
-
-    // Log their values to the console
-    console.log('FN:', firstName.value);
-    console.log('LN:', lastName.value);
-    console.log('US:', username.value);
+      openGame(handleSubmit)
 }
 
-let form = document.getElementById('login-form');
-form.addEventListener('submit', handleSubmit);
+//NEEDS REVIEWING moving a class between html items -Not working 6/11/24
+function openGame (handleSubmit) {
+  const hideWelcome = document.getElementById('welcome');
+  element.classList.add('hidden');
+  const showGame = document.getElementById('three-cups', 'selectionArea', 'submit'
+  , 'score-area');
+  element.classList.remove('hidden');
 
-function welcomePage(buttons) { 
+  console.log('I am working')
 }
-
-
-//function welcomePage(formData){}
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -53,17 +60,18 @@ function welcomePage(buttons) {
 
 //Functions
 function runGame() {
+
   if (userChoice === null){
     alert('No cup selected, please select a cup!')
     return;
   }
 
-//Array - Maybe need to add value of each BTN here rather than HTML -Potentially under picked cup if cup1 click then ===0 etc..
+//Array
   let cupNumbers = [0,1,2];
 
 //choose winning cup
     let winningCup = Math.floor(Math.random()*cupNumbers.length);
-    console.log(winningCup,cupNumbers[winningCup]); // Could one element = cup number and if the same then winner else loser
+    console.log(winningCup,cupNumbers[winningCup]); 
     compareNumber(winningCup)
 } 
 
@@ -96,9 +104,6 @@ function incrementWrongScore() {
 /**
  * Add reset game function 
  * Add reset choice function (after every submit users choice becomes null again)
- * Add game functions for best of 5, 7 unlimited 
- * Add Score Board 
- * Add Enter game screen with user form entry 
  * All screens to be apart of a singular HTML file 
  * Add game over screen
  */
