e7cebb20ed3e84d42179eb4940881db015842e7d
Daniel Whittingham <zy1pk0z7@students.codeinstitute.net>
2024-11-11 21:18:20 +0000

    Code tidied up and simplified

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,89 +2,90 @@
 // Get the button elements and add event listeners to them
 
 var userChoice = null;
-var userScore = 0;
+var gamesPlayed = 0;
 var gameChoice = 0;
+var correctGuess = 0;
+var incorrectGuess = 0;
+
+const welcomePage = document.getElementById('welcome');
+welcomePage.classList.remove('hidden');
+const gamePlayPage = document.getElementById('gameScreen');
+gamePlayPage.classList.add('hidden');
+const gameOverPage = document.getElementById('gameOverScreen');
+gameOverPage.classList.add('hidden');
 
 
 //Welcome Page and game play buttons
-document.addEventListener('DOMContentLoaded',function() {
-    let buttons = document.getElementsByTagName('button');
+document.addEventListener('DOMContentLoaded', function () {
+  let buttons = document.getElementsByTagName('button');
 
-    for (let button of buttons) {
-        button.addEventListener('click', function(){
-         if (this.getAttribute('type')=== 'submitWelcome') {
-            handleSubmit();
-          } else if (this.getAttribute('data-type') === 'submitCup') {
-                alert ('You Clicked Submit!');
-                runGame();
-            } else if (this.getAttribute('data-type')==='cupChoice'){
-              userChoice = button.value;
-          } 
-       })
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (this.getAttribute('type') === 'submitWelcome') {
+        handleSubmit();
+      } else if (this.getAttribute('data-type') === 'submitCup') {
+        alert('You Clicked Submit!');
+        runGame();
+      } else if (this.getAttribute('data-type') === 'cupChoice') {
+        userChoice = button.value;
+      }
+    })
   }
 })
 
 //Number of games 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
   let radios = this.getElementsByClassName('radio');
 
   for (let radio of radios) {
-    radio.addEventListener('click', function() {
-      if (this.getAttribute('data-type') === 'bestOfThree' ) {
-      gameChoice = 3;
-    } else if(this.getAttribute('data-type') === 'bestOfFive') {
-      gameChoice = 5;
-    }else if(this.getAttribute('data-type') === 'bestOfHundred') {
-      gameChoice = 100;
-    }
+    radio.addEventListener('click', function () {
+      if (this.getAttribute('data-type') === 'bestOfThree') {
+        gameChoice = 3;
+      } else if (this.getAttribute('data-type') === 'bestOfFive') {
+        gameChoice = 5;
+      } else if (this.getAttribute('data-type') === 'bestOfHundred') {
+        gameChoice = 100;
+      }
     })
   }
 })
 
 //Reset/restart event listeners
-document.addEventListener('DOMContentLoaded',function(){
-  let reset =this.getElementsByClassName('playOn');
+document.addEventListener('DOMContentLoaded', function () {
+  let reset = this.getElementsByClassName('playOn');
 
-  for (playOn of reset){
-    playOn.addEventListener('click', function(){
-     if (this.getAttribute('data-type')=== 'home'){
-      const home = document.getElementById('welcome', 'threeCups', 'selectionArea', 'submit'
-        , 'scoreArea', 'keepGoing');
-        welcome.classList.remove('hidden');
-        threeCups.classList.add('hidden');
-        selectionArea.classList.add('hidden');
-        submit.classList.add('hidden');
-        scoreArea.classList.add('hidden');
-        endGame.classList.add('hidden');
-        homeBtn.classList.add('hidden');
+  for (playOn of reset) {
+    playOn.addEventListener('click', function () {
+      if (this.getAttribute('data-type') === 'home') {
+        welcomePage.classList.remove('hidden');
+        gamePlayPage.classList.add('hidden');
+        gameOverPage.classList.add('hidden');
         var ele = document.getElementsByName("bestOf");
-          for(var i=0;i<ele.length;i++)
-           ele[i].checked = false;
+        for (var i = 0; i < ele.length; i++)
+          ele[i].checked = false;
+
       }
-      
     })
   }
 })
 
+
+
 let form = document.getElementById('login-form');
-form.addEventListener('submit', handleSubmit); 
+form.addEventListener('submit', handleSubmit);
 
 //Form Filled 
 function handleSubmit(event) {
   event.preventDefault();
-      let username = document.getElementById('username').value;
-      alert(`Welcome ${username}`); 
-      openGame ();
+  let username = document.getElementById('username').value;
+  alert(`Welcome ${username}`);
+  openGame();
 }
 
-function openGame () {
-  const hideWelcome = document.getElementById('welcome', 'threeCups', 'selectionArea', 'submit'
-  , 'scoreArea');
-  welcome.classList.add('hidden');
-  threeCups.classList.remove('hidden');
-  selectionArea.classList.remove('hidden');
-  submit.classList.remove('hidden');
-  scoreArea.classList.remove('hidden');
+function openGame() {
+  welcomePage.classList.add('hidden');
+  gamePlayPage.classList.remove('hidden');
+  gameOverPage.classList.add('hidden');
 }
 
 /**
@@ -93,62 +94,66 @@ function openGame () {
  */
 function runGame() {
 
-  if (userChoice === null){
+  if (userChoice === null) {
     alert('No cup selected, please select a cup!')
     return;
   }
 
-//Array
-  let cupNumbers = [0,1,2];
+  //Array
+  let cupNumbers = [1, 2, 3];
 
-//choose winning cup
-    let winningCup = Math.floor(Math.random()*cupNumbers.length);
-    //console.log(winningCup,cupNumbers[winningCup]); 
-    compareNumber(winningCup)
-} 
+  //choose winning cup
+  let winningCup = Math.floor(Math.random() * cupNumbers.length);
+
+  if (gamesPlayed < gameChoice && userChoice) {
+    compareNumber(winningCup);
+    userChoice = null;
+    gamesPlayed++
+  }
+  if (gamesPlayed === gameChoice) {
+    gameOver();
+  }
+}
 
 //Winning cup 
 function compareNumber(winningCup) {
   if (userChoice == winningCup) {
     incrementScore();
-    alert('You Won')
-    scoreCount();
+    alert('You Won');
   } else {
     alert('Oh no, better luck next time!');
     incrementWrongScore();
-    scoreCount();
   }
-    
+  //scoreCount();
 }
 
 //Score
 function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+  document.getElementById("score").innerText = ++correctGuess;
+
+
+
 }
 
 function incrementWrongScore() {
-  let oldScore = parseInt(document.getElementById("incorrect").innerText);
-  document.getElementById("incorrect").innerText = ++oldScore;
-}
-
-function scoreCount (incrementScore, incrementWrongScore) {
-  let currentScore = parseInt(userScore);
-  currentScore = userScore = ++currentScore;
-  if (currentScore === gameChoice){
-    gameOver()
-  };
+  document.getElementById("incorrect").innerText = ++incorrectGuess;
 }
 
 //GameOver
-function gameOver(){
-  const gameOverScreen = document.getElementById('welcome', 'threeCups', 'selectionArea', 'submit'
-  , 'scoreArea', 'keepGoing');
-  welcome.classList.add('hidden');
-  threeCups.classList.add('hidden');
-  selectionArea.classList.add('hidden');
-  submit.classList.add('hidden');
-  scoreArea.classList.add('hidden');
-  endGame.classList.remove('hidden');
-  homeBtn.classList.remove('hidden');
+function gameOver() {
+  welcomePage.classList.add('hidden');
+  gamePlayPage.classList.add('hidden');
+  gameOverPage.classList.remove('hidden');
+  resetScores();
+}
+
+function resetScores() {
+  userChoice = null;
+  correctGuess = 0;
+  incorrectGuess = 0;
+  gameChoice = 0;
+  gamesPlayed = 0;
+
+  document.getElementById("incorrect").innerText = incorrectGuess;
+  document.getElementById("score").innerText = correctGuess;
 }
